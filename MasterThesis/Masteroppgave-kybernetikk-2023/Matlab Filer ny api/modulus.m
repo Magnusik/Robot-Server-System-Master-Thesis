144d0afbf9186803e8f01836d228175fc743b8ab
Magnus Isdal Kolbeinsen <magnusik@ntnu.no>
2023-03-13 14:56:37 +0100

    folder name changes

--- MasterThesis/Masteroppgave-kybernetikk-2023/Matlab Filer ny api/modulus.m
+++
@@ -1,5 +0,0 @@
-function [leftover] = modulus(angle,arg)
-%modulus function
-leftover = angle - (arg*floor(angle/arg));
-end
-
--- MasterThesis/Masteroppgave-kybernetikk-2023/Matlab Filer ny api/test_moveForward.m
+++
@@ -1,7 +0,0 @@
-distanceRemaining = 100;
-distanceDriven       = 1000;
-drThreshold = 50;
-ddThreshold = 1200;
-u = 15;
-
-[uL,uR] = moveForward(distanceRemaining,distanceDriven,drThreshold,ddThreshold,u)
