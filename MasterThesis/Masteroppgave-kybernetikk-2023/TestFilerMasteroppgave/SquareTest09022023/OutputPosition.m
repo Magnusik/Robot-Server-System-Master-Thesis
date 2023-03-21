55f77ddc3a7060c8b5378710e3b3725266e6facb
Magnus Isdal Kolbeinsen <magnusik@ntnu.no>
2023-03-21 14:36:57 +0100

    added some test files and plots

---
+++ MasterThesis/Masteroppgave-kybernetikk-2023/TestFilerMasteroppgave/SquareTest09022023/OutputPosition.m
@@ -0,0 +1,25 @@
+fileID = fopen('Squaretest1.txt','r');
+
+
+tline=fgetl(fileID);
+%headers = strsplit(tline,','); kan brukes for å fjerne headers (fjerner
+%første linje
+
+formatSpec = '%f%f%f%f%f%f%f%f';
+datacell = textscan(fileID,formatSpec,'Delimiter',',');
+time = datacell{2};
+X = datacell{6};
+Z = datacell{7};
+Y = datacell{8};
+
+figure(2);
+hold on;
+plot(X(1,1),Y(1,1),'o','MarkerSize',10);
+plot(X(end,1),Y(end,1),'o','MarkerSize',10);
+plot(X,Y);
+grid on;
+title('Position Square Test 300 mm');
+xlabel('x [mm]');
+ylabel('y [mm]');
+legend('Starting point','End point','OptiTrac path');
+hold off;
---
+++ MasterThesis/Masteroppgave-kybernetikk-2023/TestFilerMasteroppgave/SquareTest300_16032023_IMU_slowdown/OutputPosition.m
@@ -0,0 +1,25 @@
+fileID = fopen('SquareTest300_16_03_23.txt','r');
+
+
+tline=fgetl(fileID);
+%headers = strsplit(tline,','); kan brukes for å fjerne headers (fjerner
+%første linje
+
+formatSpec = '%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f';
+datacell = textscan(fileID,formatSpec,'Delimiter',',');
+time = datacell{2};
+X = datacell{18};
+Z = datacell{19};
+Y = datacell{20};
+
+figure(1);
+hold on;
+plot(X(1,1),Y(1,1),'o','MarkerSize',10);
+plot(X(end,1),Y(end,1),'o','MarkerSize',10);
+plot(X,Y);
+grid on;
+title('Position Square Test 300mm');
+xlabel('x [mm]');
+ylabel('y [mm]');
+legend('Starting point','End point','OptiTrac path');
+hold off;
